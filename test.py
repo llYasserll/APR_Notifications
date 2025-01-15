8a185b6c6fdcab887fa5cff217d11e66f608bfe5
llYasserll <yasserantezana@gmail.com>
2025-01-15 15:46:16 -0500

    Test

---
+++ test.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+driver = webdriver.Chrome()
+
+driver.get("https://www.gate.io/es/startup-mining")
+driver.maximize_window()
+time.sleep(5)
+print(driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/div[3]/div[1]/div/div[1]/div[3]/div/div[2]/div[1]/div[1]/div").text)
+
+driver.quit()
