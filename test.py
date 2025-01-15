e1fcc0fe2fa1bde19000301b756d0f50f9053fe5
llYasserll <yasserantezana@gmail.com>
2025-01-15 16:35:28 -0500

    Test

--- test.py
+++ test.py
@@ -4,12 +4,14 @@ from selenium.webdriver.edge.options import Options
 import time
 
 edge_options = Options()
-edge_options.add_argument("--headless")
+#edge_options.add_argument("--headless")
 
 driver = webdriver.Edge(options=edge_options)
 
 driver.get("https://www.gate.io/es/startup-mining")
 driver.maximize_window()
+time.sleep(10)
+driver.execute_script("window.scrollBy(0, 300);")
 time.sleep(5)
 print(driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/div[3]/div[1]/div/div[1]/div[3]/div/div[2]/div[1]/div[1]/div").text)
 
