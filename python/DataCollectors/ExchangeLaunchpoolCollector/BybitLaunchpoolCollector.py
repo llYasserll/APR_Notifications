915bd518e552a1599105aba217e8cc840a312ae7
llYasserll <yasserantezana@gmail.com>
2025-05-01 18:15:15 -0500

    Update comming soon launchpools

--- python/DataCollectors/ExchangeLaunchpoolCollector/BybitLaunchpoolCollector.py
+++ python/DataCollectors/ExchangeLaunchpoolCollector/BybitLaunchpoolCollector.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.options import Options
 import logging
-
 import time
 
 driver = webdriver.Edge()
@@ -18,33 +17,26 @@ logging.basicConfig(
 )
 
 elements_earntokens=len(driver.find_elements(By.XPATH, "(//div[contains(@class, 'LaunchPoolPage')])[1]/div[3]/div[contains(@class,'NoEndActivites')]"))
-elements=len(driver.find_elements(By.XPATH, "(//div[contains(@class, 'LaunchPoolPage')])[1]/div[3]/div[contains(@class,'NoEndActivites')]"))
 
 i=0
-
 while i < elements_earntokens:
     
-    #pool = driver.find_element(By.XPATH, f"(//div[contains(@class, 'LaunchPoolPage')])[1]/div[3]/div[contains(@class,'NoEndActivites')][{i+1}]/div[2]/div/div[2]/div[2]/div[{j+1}]/div[1]/div[1]/div/div[1]/div[1]")
-    #apr = driver.find_element(By.XPATH, f"(//div[contains(@class, 'LaunchPoolPage')])[1]/div[3]/div[contains(@class,'NoEndActivites')][{i+1}]/div[2]/div/div[2]/div[2]/div[{j+1}]/div[1]/div/div[2]/div[3]/div[1]/div[2]/div")
-    #logging.info(pool)
-
     elements_token= len(driver.find_elements(By.XPATH, f"(//div[contains(@class, 'LaunchPoolPage')])[1]/div[3]/div[contains(@class,'NoEndActivites')][{i+1}]/div[2]/div/div[2]/div[2]/div/div[1]/div[1]/div/div[1]/div[1]"))
+    token= driver.find_element(By.XPATH, f"(//div[contains(@class, 'LaunchPoolPage')])[1]/div[3]/div[contains(@class,'NoEndActivites')][{i+1}]/div[2]/div/div[2]/div[1]/div[1]/div[1]/div[1]/span")
+    print(f"token -> {token.text}")
+
     j=0
     while j < elements_token:
-        token = driver.find_element(By.XPATH, f"(//div[contains(@class, 'LaunchPoolPage')])[1]/div[3]/div[contains(@class,'NoEndActivites')][{i+1}]/div[2]/div/div[2]/div[2]/div[{j+1}]/div[1]/div[1]/div[1]/div[1]/div[1]")
+        pool = driver.find_element(By.XPATH, f"(//div[contains(@class, 'LaunchPoolPage')])[1]/div[3]/div[contains(@class,'NoEndActivites')][{i+1}]/div[2]/div/div[2]/div[2]/div[{j+1}]/div[1]/div[1]/div[1]/div[1]/div[1]")
+        apr = driver.find_element(By.XPATH, f"(//div[contains(@class, 'LaunchPoolPage')])[1]/div[3]/div[contains(@class,'NoEndActivites')][{i+1}]/div[2]/div/div[2]/div[2]/div[{j+1}]/div[1]/div[1]/div[2]/div[3]/div[1]/div[2]/div")
+        total_staked= driver.find_element(By.XPATH, f"(//div[contains(@class, 'LaunchPoolPage')])[1]/div[3]/div[contains(@class,'NoEndActivites')][{i+1}]/div[2]/div/div[2]/div[2]/div[{j+1}]/div[1]/div[1]/div[2]/div[3]/div[1]/div[2]/div")
+        participants= driver.find_element(By.XPATH, f"(//div[contains(@class, 'LaunchPoolPage')])[1]/div[3]/div[contains(@class,'NoEndActivites')][{i+1}]/div[2]/div/div[2]/div[2]/div[{j+1}]/div[1]/div[1]/div[2]/div[3]/div[3]/div[2]")
+        
+        print(f"pool -> {pool.text}, apr -> {apr.text}, total staked -> {total_staked.text}, participants -> {participants.text}" )
+
         j+=1
-        print(f"token -> {token.text}" )
     
-    #print(f"pool -> {pool.text}")
-    #print(f"apr -> {apr.text}")
     i+=1
-    ##
 
-
-
-
-
-
-##
 
 
